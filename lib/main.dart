3fded29e3e08e9bcc2df235d828f7375267a727a
andresperezfc <felipe3apc@gmail.com>
2020-11-17 22:59:27 -0500

    Primer Commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:stripe_app/pages/home_page.dart';
+import 'package:stripe_app/pages/pago_completo_page.dart';
+import 'package:stripe_app/pages/tarjeta_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'StripeApp',
+      initialRoute: 'home',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light().copyWith(
+          primaryColor: Color(0xff5abd8c),
+          scaffoldBackgroundColor: Color(0xff21232A)),
+      routes: {
+        'home': (_) => HomePage(),
+        'pago_completo': (_) => PagoCompletoPage(),
+        'tarjeta': (_) => TarjetaPage()
+      },
+    );
+  }
+}
